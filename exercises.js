13f5b3d1ecce3e69016369b0dfe4daa2b454d8bc
Gregg Bellofatto <greggbellofatto@Greggs-MacBook-Pro.local>
2022-10-05 13:52:14 -0400

    finished array practice

--- exercises.js
+++ exercises.js
@@ -1,15 +1,25 @@
 // ---------------------
-// Define a function that takes a string and an integer of i and creates a new array of length i where each value is equal to the string passed in
+// Define a function that takes a string and an integer of i and creates a 
+// new array of length i where each value is equal to the string passed in
+
 // myFunction('sunshine', 3) => ['sunshine', 'sunshine', 'sunshine'];
 // ---------------------
 
 // Put your answer below -------------------------
 
 
-
+function array(str, i) {
+    const result = [];
+    for(let j = 0; j < i; j++){
+        result.push(str);
+    }
+    return result
+}
 
 
 // -----------------------------------------------
+
+
 
 // ---------------------
 // Define a function that takes an array and reverses all the values in an array
@@ -19,18 +29,17 @@
 // Put your answer below -------------------------
 
 
-const numbers = [1,2,3];
-
-const reversed = numbers.slice().reverse();
-    // console.log(reversed);
-
-
-
-
-
-
+    function array(arr) {
+        const reversed = arr.slice().reverse(); // return takes the new variable "reversed" and will give take the
+                                                // inputed "arr", will use method ".slice" to make a shallow copy of the
+                                                // "arr" but keep origianl, and then is reversed with method .reverse.
+        return reversed
+    }
 
 // -----------------------------------------------
+
+
+
 
 // ---------------------
 // Define a function that takes an array and removes all falsy values from the array
@@ -38,19 +47,15 @@ const reversed = numbers.slice().reverse();
 
 // Put your answer below -------------------------
 
-let arr = [ 0, 1, '', undefined, false, 2, undefined, null, , 3, NaN ];
- 
-let filtered = arr.filter(Boolean);
-// console.log(filtered);
-
-
-
-
-
-
-
+function array(arr) { 
+    return arr.filter(Boolean) // return takes the "arr" and uses the filter 
+                               // method ".filter" and returns the truthy values using "(Boolean)"
+}
 
 // -----------------------------------------------
+
+
+
 
 // ---------------------
 // Define a function that takes an array of nested arrays and returns an object composed of propeties equal to each nested array
@@ -60,18 +65,18 @@ let filtered = arr.filter(Boolean);
 
 // Put your answer below -------------------------
 
-// const myArray = [['name', 'Charlie'], ['color', 'brown'], ['age', 10]];
-
-// 
-
-
- 
-
-
-
-
+function answer1(arr) {
+    const result = {};                      // first, makes an empty object 
+    for(let i = 0; i < arr.length; i++){    // use for loop to pass through each value in nested array (arr.length)
+        result[arr[i][0]] = arr[i][1];      // result brings in [name, "charlie"......]
+    }
+    return result
+}
 
 // -----------------------------------------------
+
+
+
 
 // ---------------------
 // Define a function that takes an array and removes duplicate values
@@ -80,19 +85,17 @@ let filtered = arr.filter(Boolean);
 
 // Put your answer below -------------------------
 
-
-let numArray = [1,2,3,4,5,4,3];
-let removes = [...new Set(numArray)];
-
-// console.log(removes);
-
-
-
-
-
-
+function array(arr) {
+    let newArray = [new Set(arr)]; // <--- here we define a new varialbe and create a new Set. We then pass in the 
+                                    // original array and a value of "Set" will occur only once.
+    return newArray
+}
 
 // -----------------------------------------------
+
+
+
+
 
 // ---------------------
 // Define a function that takes two arrays and returns true if they have identical values (order does not matter), it should return false otherwise
@@ -104,28 +107,19 @@ let removes = [...new Set(numArray)];
 
 // Put your answer below -------------------------
 
-function areEqual(arr1, arr2)
-    {
-        let N = arr1.length;    // If lengths of array are not equal means
-        let M = arr2.length;    // array are not equal  
-            if (N != M)
-            return false;
-        arr1.sort();                // Sort both arrays
-        arr2.sort();
-         for (let i = 0; i < N; i++)    // Linearly compare elements 
-            if (arr1[i] != arr2[i])
-            return false;
-            return true;           // If all elements were same.
-
-     }
-        let arr1 = [1,2,3,4];      // Driver Code
-        let arr2 = [1,4,3,2];
-        if (areEqual(arr1, arr2))
-            document.write("Yes");
-        else
-            document.write("No");
-
-
+function areEqual(arr1, arr2) {
+    let N = arr1.length;    // make two variables for each array to check the length
+    let M = arr2.length;    
+        if (N != M)         // a quick test to return false for not equal length arrays
+        return false;
+    arr1.sort();                // now we can take each array and use the ".sort" method to arrange in array in an order.
+    arr2.sort();                
+        for (let i = 0; i < N; i++)    // then we can use a for loop to test each array against each other.
+        if (arr1[i] != arr2[i])         // if the vaules are not the same 
+        return false;                   // we will return false
+        return true;                    // if they are equal we will return turn.
+    }
+    
 
 // -----------------------------------------------
 
@@ -149,15 +143,14 @@ function areEqual(arr1, arr2)
 // Put your answer below -------------------------
 
 
-
-
-
-
-
-
-
+function array(arr1, arr2) {
+    return arr1.concat(arr2) //used .concat to merge arrays together 
+}
 
 // -----------------------------------------------
+
+
+
 
 // ---------------------
 // Define a function that takes an array and splits it into parts of size i
@@ -167,13 +160,14 @@ function areEqual(arr1, arr2)
 
 // Put your answer below -------------------------
 
+function array(arr, size) {   // we are taking an inputed array "arr", and then inputed the "size" of the array we want
+    const result = [];        // need to make a variable to set an empty array
 
-
-
-
-
-
-
-
+    for(let i = 0; i < arr.length; i += size) {   // using for loop to iterate over the array length 
+        const portion = arr.slice(i, i + size);   // set a new variable which will take the array, make a shallow copy and set to the new array
+        portion.push(size);                       // use the .push method will add the shallow copy to the end
+    }
+    return result
+}
 
 // -----------------------------------------------
